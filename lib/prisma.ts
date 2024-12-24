9a4fa4b5f018d63b78f2e2dccbeaf3d42d226065
Selenum-token <selenum.service@gmail.com>
2024-12-24 14:43:05 +0300

    New version 5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+const prismaClientSingleton = () => {
+	return new PrismaClient()
+}
+
+declare global {
+	var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
